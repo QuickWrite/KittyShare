1285752f107e64a764b41bd45ab8d488a1c62437
QuickWrite <git@quickwrite.net>
2026-09-16 19:22:06 +0200

    Add basic styles to the application
    The styles are currently not ready, however they provide a simple base.

--- src/Template/partials/base.php
+++ src/Template/partials/base.php
@@ -16,13 +16,26 @@ function renderHeader(string $title): void {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e($title) ?> | KittyShare</title>
+
+    <!-- TODO: Add base path to enable other configuations than just the root -->
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
+    <header class="header">
+        <div class="header--icon">
+            <img src="/icon/kittyshare-logo-light.svg" alt="" /> <span>KittyShare</span>
+        </div>
+    </header>
 <?php
 }
 
 function renderFooter(): void {
 ?>
+    <footer class="footer">
+        <span class="footer--note">
+            Powered by <a href="https://github.com/QuickWrite/KittyShare">KittyShare</a>.
+        </span>
+    </footer>
 </body>
 </html>
 <?php
